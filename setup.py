ed69b4bcf2cd5bcdb7bb83f59a5b70eb5e419b65
Léo Lam <leo@innovatetechnologi.es>
2018-10-06 13:57:45 +0200

    Use versioneer for versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import fastentrypoints
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="botwfstools",
-    version="1.4.0",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="leoetlino",
     author_email="leo@leolam.fr",
     description="Tools for exploring and editing Breath of the Wild's ROM",
